a57e5dfd4c235905b5a913fc9d1a436307be320c
Arkariza <zex3375@gmail.com>
2026-06-06 00:53:33 +0700

    walahi

--- Routes/ScannerR.js
+++ Routes/ScannerR.js
@@ -5,6 +5,12 @@ const ScannerController = require("../Controllers/LogAbsenController")
 const auth = require("../Middleware/AuthMiddleware")
 const role = require("../Middleware/RoleMiddleware")
 
-router.post("/scan", auth, ScannerController.scanQR)
+router.post("/scan", (req, res) => {
+  return res.status(200).json({
+    message: "ROUTE SCANNER BARU KEBACA",
+    body: req.body,
+    waktu: new Date(),
+  })
+})
 
 module.exports = router
